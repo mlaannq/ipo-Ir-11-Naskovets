9b083943dbe977407dbe3572afa9fc7c34c4552a
mlaannq <naskovecmilana@gmail.com>
2024-12-22 23:20:57 +0300

    Update Client.py

--- transport/Client.py
+++ transport/Client.py
@@ -1,15 +1,16 @@
+import re #для ипсользования регулярного выражения
 class Client:
     def __init__(self, name, cargo_weight, is_vip=False):
-        if name:
-            self.name = name
-        else:
-            raise ValueError("Имя клиента должно быть указано")
+        #проверка имени клиента
+        if not name or not re.match("[A-Za-z]", name): #шаблон из каких символов может состоять имя клиента (регулярное вырожение)
+            raise ValueError("Имя клиента должно содержать только буквы и не должно быть пустым")
+
+        self.name = name  #убираем лишние пробелы
+        #проверка веса груза
         if not isinstance(cargo_weight, (int, float)) or cargo_weight < 0:
             raise ValueError("Вес груза должен быть положительным ЧИСЛОМ")
-        if not (is_vip == True or is_vip == False):
+        #проверка vip статуса
+        if not isinstance(is_vip, bool):
             raise ValueError("VIP-статус должен быть True или False")
-        self.is_vip = bool(is_vip); self.name = name.strip(); self.cargo_weight = cargo_weight; self.is_vip = is_vip
-
-
-def lient():
-    return None
+        self.cargo_weight = cargo_weight
+        self.is_vip = is_vip
